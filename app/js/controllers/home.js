cb48c0b0d493d64ed30c34a349d068e53aa061cd
Ryan McQuen <rpcm@linux.com>
2020-09-11 09:37:58 -0700

    Use more efficient word counting.

--- app/js/controllers/home.js
+++ app/js/controllers/home.js
@@ -121,10 +121,7 @@ class HomeCtrl {
 }
 
 function countWords(s) {
-  s = s.replace(/(^\s*)|(\s*$)/gi, ""); //exclude  start and end white-space
-  s = s.replace(/[ ]{2,}/gi, " "); //2 or more space to 1
-  s = s.replace(/\n /, "\n"); // exclude newline with a start spacing
-  return s.split(' ').length;
+  return s.match(/\b/gm).length / 2;
 }
 
 function downloadText(filename, text) {
